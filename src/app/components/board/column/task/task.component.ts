e4e8e8eae6c55e6b48bceeb6478114a41b8d0b26
MoaresOliveira <moaresoliveira@gmail.com>
2022-06-10 22:01:40 -0300

    Edit and Delete from To Do Column Done

--- src/app/components/board/column/task/task.component.ts
+++ src/app/components/board/column/task/task.component.ts
@@ -1,37 +1,87 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  EventEmitter,
+  Input,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-task',
-  templateUrl: './task.component.html'
+  templateUrl: './task.component.html',
 })
 export class TaskComponent implements OnInit {
-
+  @Input() id: number = 1;
   @Input() title: string = 'Task';
   @Input() description: string = 'Description';
+  @Input() editable = true;
+  @Output() onEdit = new EventEmitter();
+  @Output() onDelete = new EventEmitter();
+
+  @ViewChild('inputTitle') inputTitle: ElementRef;
+  @ViewChild('inputDescription') inputDescription: ElementRef;
+
+  editing: boolean = false;
   date: Date = new Date();
 
-  constructor() { }
+  constructor(inputTitleRef: ElementRef, inputDescriptionRef: ElementRef) {
+    this.inputTitle = inputTitleRef;
+    this.inputDescription = inputDescriptionRef;
+  }
 
   ngOnInit(): void {
-    if(this.title.trim().length == 0){
-      this.title = 'Task'
+    if (this.title.trim().length == 0) {
+      this.title = 'Task';
     }
   }
 
-  getDate(){
+  editTask() {
+    this.editing = true;
+    console.log(this.inputTitle);
+  }
+
+  saveEdition() {
+    this.editing = false;
+    this.title = this.inputTitle.nativeElement.value;
+    this.description = this.inputDescription.nativeElement.value;
+    this.onEdit.emit({
+      task: {
+        id: this.id,
+        name: this.title,
+        description: this.description,
+      },
+    });
+  }
+
+  cancelEdition() {
+    this.editing = false;
+  }
+
+  deleteTask() {
+    this.onDelete.emit({
+      task: {
+        id: this.id,
+        name: this.title,
+        description: this.description,
+      },
+    });
+  }
+
+  getDate() {
     let day = this.formatNumber(this.date.getDate());
     let month = this.formatNumber(this.date.getMonth() + 1);
     let year = this.formatNumber(this.date.getFullYear());
 
-    return `${day}/${month}/${year}`
+    return `${day}/${month}/${year}`;
   }
 
-  formatNumber(number: number){
-    if(number < 10){
+  formatNumber(number: number) {
+    if (number < 10) {
       return '0' + number;
-    }else{
+    } else {
       return number.toString();
     }
   }
-
 }
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,61 +1,100 @@
-import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDragDrop,
+  CdkDropList,
+  moveItemInArray,
+  transferArrayItem,
+} from '@angular/cdk/drag-drop';
 import { Component } from '@angular/core';
+import { ColumnComponent } from '../board/column/column.component';
 import { Task } from '../board/column/task/task.type';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.css']
+  styleUrls: ['./todo-list.component.css'],
 })
 export class TodoListComponent {
-
   title = 'ToDoList';
 
-  tasksToDo: Task[] = [
-    {
-      name: 'task 1',
-      description: 'a'
-    },
-    {
-      name: 'task 2',
-      description: 'a'
-    },
-    {
-      name: 'task 3',
-      description: 'a'
-    }
-  ]
-  tasksDoing: Task[] = []
-  tasksDone: Task[] = []
+  columns:
+  {tasksToDo: Task[]; tasksDoing: Task[]; tasksDone: Task[]} = {
+    tasksToDo:  [
+      {
+        id: 1,
+        name: 'task 1',
+        description: 'a',
+      },
+      {
+        id: 2,
+        name: 'task 2',
+        description: 'a',
+      },
+      {
+        id: 3,
+        name: 'task 3',
+        description: 'a',
+      },
+    ],
+    tasksDoing: [],
+    tasksDone: []
+  }
+
 
   taskToSave: Task = {
+    id: 1,
     name: '',
-    description: ''
-  }
+    description: '',
+  };
 
-
-  addTask(){
-    if(this.validate()){
-      this.tasksToDo.push({...this.taskToSave})
+  addTask() {
+    if (this.validate()) {
+      this.columns['tasksToDo'].push({ ...this.taskToSave });
     }
+    this.taskToSave.id++;
   }
 
-  onDrop(event: CdkDragDrop<Task[]>){
-    if(event.previousContainer === event.container){
-      moveItemInArray(event.container.data,event.previousIndex,event.currentIndex)
-      console.log(event.container.data)
-    }else{
-      transferArrayItem(event.previousContainer.data,event.container.data,event.previousIndex, event.currentIndex)
-      console.log(event.container.data)
+  editTask(task: Task) {
+    let index = this.columns['tasksToDo'].findIndex((item) => item.id == task.id);
+    if (index != -1) {
+      this.columns['tasksToDo'][index] = task;
+    }
+    console.log(this.columns['tasksToDo']);
+  }
+
+  deleteTask(taskDeleted: Task) {
+    this.columns['tasksToDo'] = this.columns['tasksToDo'].filter((task) => task.id != taskDeleted.id);
+  }
+
+  onDrop(event: CdkDragDrop<Task[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      console.log(event.container.data);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex
+      );
+      console.log(event.container.data);
     }
   }
 
   validate() {
-    if(this.taskToSave.name.trim().length == 0){
+    if (this.taskToSave.name.trim().length == 0) {
       return false;
-    }else if(this.taskToSave.description.trim().length == 0){
+    } else if (this.taskToSave.description.trim().length == 0) {
       return false;
-    }else{
+    } else if (this.columns['tasksToDo'].some((task) => task.id == this.taskToSave.id)) {
+      while (this.columns['tasksToDo'].some((task) => task.id == this.taskToSave.id)) {
+        this.taskToSave.id++;
+      }
+      return true;
+    } else {
       return true;
     }
   }
